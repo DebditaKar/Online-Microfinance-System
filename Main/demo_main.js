026fe064416db55ea6d2b454f081f8c72685e0f6
AROHISHIRKE <66770661+AROHISHIRKE@users.noreply.github.com>
2020-06-29 12:29:45 +0530

    Delete demo_main.js

--- Main/demo_main.js
+++
@@ -1,6 +0,0 @@
-function edit(){
-    document.getElementById("address").disabled=false;
-    document.getElementById("phone").disabled=false;
-    document.getElementById("mail").disabled=false;
-    document.getElementById("gid").disabled=false;
-}
