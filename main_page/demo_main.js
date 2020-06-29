ad905a6e4bd28001499aa14594af7979cfb08523
AROHISHIRKE <66770661+AROHISHIRKE@users.noreply.github.com>
2020-06-29 12:54:12 +0530

    Create demo_main.js

---
+++ main_page/demo_main.js
@@ -0,0 +1,8 @@
+function afterEdit() {
+    document.getElementById("address").disabled=false;
+    document.getElementById("phone").disabled=false;
+    document.getElementById("gid").disabled=false;
+    document.getElementById("password").disabled=false;
+    document.getElementById("save").style.display='block';
+    document.getElementById("edit").style.display='none';
+} 
