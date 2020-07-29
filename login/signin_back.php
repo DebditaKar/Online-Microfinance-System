4f13d940ea132fb68d8176d03b660fe1b8b1dd63
AROHISHIRKE <66770661+AROHISHIRKE@users.noreply.github.com>
2020-07-29 12:28:24 +0530

    Update signin_back.php

--- login/signin_back.php
+++ login/signin_back.php
@@ -2,19 +2,35 @@
     $a= $_POST["username"];
 	$b= $_POST["password"];
 
-        $db = new PDO("mysql:host=localhost;dbname=oms","root","");
+        $db = new PDO("mysql:host=localhost;dbname=mfs","root","");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         try 
         {
-            $query = $db->query("SELECT * FROM admin WHERE username = '{$a}' AND password = '{$b}'");
+            $query = $db->query("SELECT * FROM admin WHERE Admin_ID = '{$a}' AND Password = '{$b}'");
+            $row = $query->fetch();
+            $type = $row['Type'];
             $row_count = $query->rowCount();
             if($row_count==0)
             {
-                echo "UNSUCCESSFULL";
+                echo "<script type='text/javascript' >
+                document.location='../index.html'
+                alert('Wrong id or password')
+                </script>";
             }
             else
             {
-                echo "SUCCESSFULL";
+                if($type == 'Collector')
+                {
+                    echo "<script type='text/javascript' >
+                    document.location='../collector/collector.php'
+                    </script>";
+                }
+                if($type == 'Management')
+                {
+                    echo "<script type='text/javascript' >
+                    document.location='../admin/admin.php'
+                    </script>";
+                }
             }
         }
         catch(PDOException $e)
