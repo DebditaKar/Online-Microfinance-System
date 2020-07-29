be1cd6b46921aaea89ef719ec483399c1493ed89
AROHISHIRKE <66770661+AROHISHIRKE@users.noreply.github.com>
2020-07-29 20:23:49 +0530

    Delete demo_main.js

--- main_page/demo_main.js
+++
@@ -1,8 +0,0 @@
-function afterEdit() {
-    document.getElementById("address").disabled=false;
-    document.getElementById("phone").disabled=false;
-    document.getElementById("gid").disabled=false;
-    document.getElementById("password").disabled=false;
-    document.getElementById("save").style.display='block';
-    document.getElementById("edit").style.display='none';
-} 
